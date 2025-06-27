14736039e3280fcba6ab2f827fc24ef2320753fb
vitoriazrr <vitoriazrr@gmail.com>
2025-06-27 14:14:31 -0400

    Add files via upload
    Adicionando arquivos do podcast

---
+++ Main (1).java
@@ -0,0 +1,12 @@
+package br.com.podcast;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("🎙️ Podcast: Tecnologia e o Futuro\n");
+        System.out.println("Hoje vamos falar sobre algo que está mudando a vida de todo mundo: a tecnologia.");
+        System.out.println("A inteligência artificial está em todo lugar — nos apps, nas redes sociais e até na medicina.");
+        System.out.println("A nova geração já nasce conectada. Jovens estão criando apps e fazendo cursos online.");
+        System.out.println("Plataformas como a DIO mostram que tecnologia é para todos!");
+        System.out.println("Se você tem curiosidade, esse é o momento. O futuro já começou. 🚀");
+    }
+}
